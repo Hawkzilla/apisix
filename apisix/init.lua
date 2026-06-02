41dcd648bad183e6a4ca7feef72d5b440897c6d7
bumblebeek <gleemanx@126.com>
2026-06-02 19:19:02 +0800

    fix: remove default value assignment for enable_websocket in init.lua

--- apisix/init.lua
+++ apisix/init.lua
@@ -788,9 +788,6 @@ function _M.http_access_phase()
         api_ctx.conf_id = route.value.id
     end
 
-    if enable_websocket == nil then
-        enable_websocket = true
-    end
     api_ctx.route_id = route.value.id
     api_ctx.route_name = route.value.name
 
